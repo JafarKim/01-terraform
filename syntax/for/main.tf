eff5fb63a6312fb4454a0e6bbae42dc8a6a1f4d7
JafarKim <renzuken@naver.com>
2022-12-26 16:13:45 +0900

    20221226

---
+++ syntax/for/main.tf
@@ -0,0 +1,16 @@
+provider "local" {
+}
+
+variable "hero_thousand_faces" {
+	description = "map"
+	type = map(string)
+	default = {
+		neo = "savior"
+		trinity = "love interest"
+		morpheus = "mentor"
+	}
+}
+
+output "bios" {
+  value = [for name , role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
---
+++ web-cluster/variables.tf
@@ -0,0 +1,10 @@
+variable "server_port" {
+  description = "THE port will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "security_group_name" {
+  type    = string
+  default = "std04-terraform-example-instance"
+}
