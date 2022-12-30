3e0d752937140b1f9641d04e81e2a02e5f593f7b
JafarKim <renzuken@naver.com>
2022-12-30 12:20:33 +0900

    20221230

--- web-cluster/main.tf
+++ web-cluster/main.tf
@@ -1,6 +1,7 @@
 terraform {
+  # 테라폼 버전
   required_version = ">= 1.0.0, < 2.0.0"
-
+  # 공급자 버전
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,6 +14,7 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+# 시작 템플릿
 resource "aws_launch_template" "example" {
   name                   = "std04-example"
   image_id               = "ami-06eea3cd85e2db8ce"
@@ -27,12 +29,16 @@ resource "aws_launch_template" "example" {
   }
 }
 
+# 오토 스케일링
 resource "aws_autoscaling_group" "example" {
   availability_zones = ["ap-northeast-2a", "ap-northeast-2c"]
   name               = "std04-terraform-asg-example"
   desired_capacity   = 1
   min_size           = 1
   max_size           = 2
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   launch_template {
     id      = aws_launch_template.example.id
@@ -46,6 +52,7 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+# 보안 그룹
 resource "aws_security_group" "instance" {
   name = var.security_group_name
 
@@ -56,6 +63,87 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# 보안 그룹 : 로드밸런서, 80 포트 오픈
+resource "aws_security_group" "alb" {
+  name = "std04-terraform-example"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 로드 밸런서
+resource "aws_lb" "example" {
+  name               = "std04-terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# 대상 그룹
+resource "aws_lb_target_group" "asg" {
+  name     = "std04-terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+
+# LB 리스너 정의
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# LB 리스너 규칙
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
 
 data "aws_vpc" "default" {
   default = true
