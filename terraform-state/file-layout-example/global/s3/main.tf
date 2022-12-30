305723784f581e1f928db8bc1af8181d4312d07a
JafarKim <renzuken@naver.com>
2022-12-30 16:11:14 +0900

    20221230

---
+++ terraform-state/file-layout-example/global/s3/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# 원격 상태 광리 저장소로 S3을 사용
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "std04-terraform-state"
+
+  # 버킷의 삭제를 허용하지 않음
+  #lifecycle {
+  #  prevent_destroy = true
+  #}
+  force_destroy = true
+}
+
+# 코드이력을 관리하기 위한 파일의 버전관리를 활성화
+resource "aws_s3_bucket_versioning" "terraform_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# 서버측 암호화를 활성화한다.
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "std04-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
